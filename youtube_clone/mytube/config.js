9ae16affde47d25942bdcdd9e22a712c27917772
Osmel <oprades4@gmail.com>
2025-01-02 20:31:44 -0500

    ss

--- youtube_clone/mytube/config.js
+++ youtube_clone/mytube/config.js
@@ -1,3 +1,5 @@
 // export const url = "http://localhost:4000" 
-export const url = 'http://backend-seven-delta-45.vercel.app/'
+// export const url = 'http://backend-seven-delta-45.vercel.app/'
+export const url = "https://backend-ciga.onrender.com"
+// https://backend-ciga.onrender.com
 // http://backend-seven-delta-45.vercel.app/
